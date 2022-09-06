a7577f891e7f03f005134284344da191fe1efc65
NickLegacy <nicklegacy13@gmail.com>
2022-09-06 19:53:15 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool isCheck)
+        {
+            var sum = x + y;
+            if (isCheck == true && sum == 1)
+            {
+                return ($"{sum} dollar");
+            }
+            if (isCheck == true && sum != 1)
+            {
+                return ($"{sum} dollars");
+            }
+            else
+                return x + y.ToString();
+            }
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
